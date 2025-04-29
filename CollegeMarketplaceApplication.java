adb1cf96836f6bf6ab087aae2c1782ec165e9b71
itztanmayy <theweddingjourney10@gmail.com>
2025-04-29 13:56:59 +0530

    Add files via upload

---
+++ CollegeMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.collegemarketplace.college_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMarketplaceApplication.class, args);
+	}
+
+}
