5d2b9ad9cf4d5a2358ae43d991bcdc9afdc6f4a8
itztanmayy <theweddingjourney10@gmail.com>
2025-04-30 00:17:04 +0530

    Updated App.jsx

--- App.jsx
+++ App.jsx
@@ -2,16 +2,28 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Register from "./Register";
 import Signin from "./Signin";
-import Sell from './sell';
+import Sell from './Sell';
+import ProductDetail from './ProductDetail';
+import Electronics from './Electronics';
+import Sports from "./Sports";
+import Health from "./Health";
+import Fashion from "./Fashion";
+import Collectibles from "./Collectibles";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Register" element={<Register />} />
-      <Route path="/Signin" element={<Signin />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/signin" element={<Signin />} />
       <Route path="/sell" element={<Sell />} />
-
+      <Route path="/product/:productId" element={<ProductDetail />} />
+      <Route path="/Electronics" element={<Electronics />} />
+      <Route path="/Collectibles" element={<Collectibles />} />
+      <Route path="/Fashion" element={<Fashion />} />
+      <Route path="/Health" element={<Health />} />
+      <Route path="/Sports" element={<Sports />} />
     </Routes>
   );
 }
